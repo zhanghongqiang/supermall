475e106780b4f79b85676b452f021f6847846c79
zhq <519662924@qq.com>
2020-08-21 14:32:45 +0800

    详情页面navbar和swiper添加

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,16 +1,38 @@
 <template>
-    <div></div>
+    <div id="detail">
+       <detail-nav-bar/>
+       <detail-swiper :topImages="topImages"></detail-swiper>
+    </div>
 </template>
 <script>
+import { getDetail } from 'network/detail'
+import DetailNavBar from './childComps/DetailNavBar'
+import DetailSwiper from './childComps/DetailSwiper'
 export default {
   name: 'Detail',
   data () {
     return {
-      iid: null
+      iid: null,
+      topImages: []
     }
+  },
+  components: {
+    DetailNavBar,
+    DetailSwiper
   },
   created () {
     this.iid = this.$route.params.iid
+    // getDetail(this.iid).then(res => {
+    //  console.log(res)
+    // })
+  },
+  methods: {
+    getDetail () {
+      getDetail(this.iid).then(res => {
+        this.topImages = res.result.itemInfo.topImages
+        console.log(res)
+      })
+    }
   }
 }
 </script>
---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <nav-bar class="detail-nav">
+        <template v-slot:left>
+            <img src="~assets/img/common/back.svg" alt="" class="back" @click="back">
+        </template>
+        <template v-slot:center>
+            <div class="title">
+                <span v-for="(item, index) in titles" :key="index" :class="{active: currentIndex === index}" @click="itemClick(index)" class="title-item">
+                    {{item}}
+                </span>
+            </div>
+        </template>
+    </nav-bar>
+</template>
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+export default {
+  name: 'DetailNavBar',
+  components: {
+    NavBar
+  },
+  data () {
+    return {
+      currentIndex: 0
+    }
+  },
+  props: {
+    titles: {
+      type: Array,
+      default () {
+        return ['商品', '参数', '评论', '推荐']
+      }
+    }
+    // currentIndex: {
+    //  type: Number,
+    //  default: 0
+    // }
+  },
+  methods: {
+    itemClick (index) {
+      this.currentIndex = index
+    },
+    back () {
+      this.$router.back()
+    }
+  }
+}
+</script>
+<style scoped>
+    .detail-nav{
+        background-color: #fff;
+    }
+    .title-item.active{
+        color: #ff5777;
+    }
+    .title{
+        display:flex;
+        padding: 0 20px;
+        font-size: 14px;
+    }
+    .title-item{
+        flex: 1;
+    }
+    .back{
+        margin-top: 12px;
+    }
+</style>
